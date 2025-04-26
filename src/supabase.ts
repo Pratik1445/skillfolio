3276e808efe3790f3ac5b59f0996dedafc8303e0
Pratik1445 <pratikchanna1445@gmail.com>
2025-04-26 22:30:05 +0530

    Update supabase.ts

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gppvgxomaviiezxsaasu.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwcHZneG9tYXZpaWV6eHNhYXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY5NDQ4NDcsImV4cCI6MjA1MjUyMDg0N30.xceU3reGBywt4OM8jmDV_EeiAOxPiEoY1805m944Kvw';
+const supabaseUrl = 'https://zbhyhuqwdnaedwxitwmn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpiaHlodXF3ZG5hZWR3eGl0d21uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2ODUyNTMsImV4cCI6MjA2MTI2MTI1M30.21NSz3J7yoUX35CnTtCTU6wx2o2yfMCbcSWOkxquKX8';
 
 export const supabase = createClient(supabaseUrl, supabaseKey); 
